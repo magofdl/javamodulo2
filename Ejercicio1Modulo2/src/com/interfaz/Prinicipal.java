ca355c56044bb7392bf775806200f68ea2d26c81
CEC <CEC@L3CEC15>
2015-05-09 10:38:04 -0500

    agregado lista de contenido de directorio

--- Ejercicio1Modulo2/src/com/interfaz/Prinicipal.java
+++ Ejercicio1Modulo2/src/com/interfaz/Prinicipal.java
@@ -16,18 +16,34 @@ public class Prinicipal {
 
     public static void main(String[] args) {
 
-        ResultadoLectura resultadoLectura = OperacionesUsuario.leerTeclado("Ingrese el path del archivo:");
+//        ResultadoLectura resultadoLectura = OperacionesUsuario.leerTeclado("Ingrese el path del archivo:");
+//        if (resultadoLectura.isLecturaCorrecta()) {
+//            List<String> listaAtributos = OperacionesArchivo.calcularAtributosArchivo(resultadoLectura.getContenido());
+//            if (listaAtributos!=null) {
+//                System.out.println(listaAtributos);
+//            }
+//            else{
+//                System.err.println("No se encontró el archivo");
+//            }       
+//        }
+//        else{
+//            System.err.println("Ocurrio un error");
+//        }
+        
+        ResultadoLectura resultadoLectura = OperacionesUsuario.leerTeclado("Ingrese el path del directorio:");
+        
         if (resultadoLectura.isLecturaCorrecta()) {
-            List<String> listaAtributos = OperacionesArchivo.calcularAtributosArchivo(resultadoLectura.getContenido());
-            if (listaAtributos!=null) {
-                System.out.println(listaAtributos);
+            List<String> listaContenido = OperacionesArchivo.listarContenidoDirectorio(resultadoLectura.getContenido());
+            if (listaContenido!=null) {
+                System.out.println(listaContenido);
+                
+//                for (String aux : listaContenido) {
+//                    System.out.println(aux);
+//                }
             }
             else{
                 System.err.println("No se encontró el archivo");
             }       
-        }
-        else{
-            System.err.println("Ocurrio un error");
         }
 
     }
